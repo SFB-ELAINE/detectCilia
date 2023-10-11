7755ff65a7ed30b1a9fc4269b23a5efcfb7f957a
buddekai <kaibudde@gmail.com>
2023-10-11 16:18:04 +0200

    add possibility to specify output directory and change directory of test images

--- inst/testscript.R
+++ inst/testscript.R
@@ -1,41 +1,12 @@
 # Testscript for using the R package detectCilia +++++++++++++++++++++++++++
 # Author: Kai Budde-Sagert
-# Last changed: 2023/10/09
+# Last changed: 2023/10/11
 
 
 # Delete everything in the environment
 rm(list = ls())
 # close all open plots in RStudio
 graphics.off()
-
-
-# Please adapt the following parameters ####################################
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-### TIF DIRECTORY ###
-
-# Directory of the images
-input_dir <- file.path("inst", "testImagesTif")
-
-# Size of a pixel in micrometer
-pixel_size <- 0.219645 # in \mu m
-
-# Distance between two layers in micrometers
-slice_distance <- 0.31607# in \mu m
-
-cilium_color <- "red"
-
-# Manually set mask width here because image is small
-nuc_mask_width_height <- 100
-
-
-### CZI FILE ###
-
-# Directory of the images
-input_file <- file.path("inst", "testImageCzi", "CiliaImage.czi")
-
-
-# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 # Install packages #########################################################
 groundhog.day <- "2023-01-01"
@@ -75,11 +46,45 @@ if(!("detectCilia" %in% installed.packages()[,"Package"])){
 require(detectCilia)
 
 
+# Please adapt the following parameters ####################################
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+### TIF DIRECTORY ###
+
+# Directory of the images
+input_dir <- system.file("extdata", "testImagesTif",
+                          package = "detectCilia", mustWork = TRUE)
+
+# Directory for the output
+output_dir <- file.path(getwd(), "output")
+
+# Size of a pixel in micrometer
+pixel_size <- 0.219645 # in \mu m
+
+# Distance between two layers in micrometers
+slice_distance <- 0.31607# in \mu m
+
+cilium_color <- "red"
+
+# Manually set mask width here because image is small
+nuc_mask_width_height <- 100
+
+
+### CZI FILE ###
+
+# Directory of the images
+input_file <- system.file("extdata", "testImageCzi", "CiliaImage.czi",
+                          package = "detectCilia", mustWork = TRUE)
+
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 ## FIRST EXAMPLE TIF DIRECTORY ---------------------------------------------
 
 # Obtain all positions of cilia in every z-layer
-detectCIlia_output_list <- detectCilia::detectCilia(
+detectCilia_output_list <- detectCilia::detectCilia(
   input_dir_tif = input_dir,
+  output_dir = output_dir,
   cilium_color = cilium_color,
   pixel_size = pixel_size,
   slice_distance = slice_distance,
@@ -88,4 +93,4 @@ detectCIlia_output_list <- detectCilia::detectCilia(
 ## SECOND EXAMPLE CZI FILE -------------------------------------------------
 
 # Obtain all positions of cilia in every z-layer
-detectCIlia_output_list2 <- detectCilia::detectCilia(input_file_czi = input_file)
+detectCilia_output_list2 <- detectCilia::detectCilia(input_file_czi = input_file)
